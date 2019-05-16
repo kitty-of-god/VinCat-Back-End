5705d5bf74a33f7f505f63ee68d9a0969a3c3f52
Daniel Angulo <dangulos@unal.edu.co>
2019-05-16 05:32:36 +0000

    validaciones completadas pero hace falta lipiar unos pocos errores

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,7 @@ class Message < ApplicationRecord
   has_one :user, through: :chat
   has_one :chat
   
-  validates :text, presence: true
+  validates :text, 
+  length: { maximum: 200},
+  presence: true
 end
