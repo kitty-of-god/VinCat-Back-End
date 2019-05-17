d3a725aeca26d78c5577fc8b758937cfb6b03da3
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2019-05-17 00:19:50 -0500

    seed user and products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  User.create!([{
+      username: Faker::Internet.username,
+      name: Faker::Name.name,
+      description: Faker::Hacker.say_something_smart,
+      password: Faker::Internet.password,
+      residence: Faker::Address.city,
+      type: "User"
+                }])
+end
+5.times do
+  Product.create!([{
+      name: Faker::Commerce.product_name,
+      description: Faker::Commerce.department,
+      price: Faker::Commerce.price,
+      quantity: 1,
+      class: "Pant",
+      gender: "Men",
+      state: true,
+      new: false,
+      user: User.first
+
+                   }])
+end
