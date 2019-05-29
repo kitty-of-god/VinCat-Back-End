4394560d8b482a19f2d53171946035f75f702c85
Gabriel Aguirre <gdaguirrea@unal.edu.co>
2019-05-28 21:42:05 -0500

    removed validations and relationships from serializers

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,9 +1,3 @@
 class MessageSerializer < ActiveModel::Serializer
   attributes :text, :chat_id
-
-    belongs_to :chat, optional: true
-
-    validates :text,
-    length: { maximum: 200},
-    presence: true
 end
