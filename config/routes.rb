d7b1a503725da345c2616f3c5e6810c66f3c5274
Daniel Angulo <dangulos@unal.edu.co>
2019-06-01 18:59:36 +0000

    simple user authentication funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -74,6 +74,7 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   get 'users/current' => 'users#current'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, only: []
   resources :carts
   resources :chats
   resources :images
