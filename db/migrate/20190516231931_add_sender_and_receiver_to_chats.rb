0d0da65c6017b4b5301956d57140d246cf82d489
Daniel Angulo <dangulos@unal.edu.co>
2019-06-03 03:21:43 +0000

    Heroku compatibilities

--- db/migrate/20190516231931_add_sender_and_receiver_to_chats.rb
+++ db/migrate/20190516231931_add_sender_and_receiver_to_chats.rb
@@ -1,6 +1,6 @@
 class AddSenderAndReceiverToChats < ActiveRecord::Migration[5.2]
   def change
-    add_reference :chats, :sender, user: true, foreign_key: true
-    add_reference :chats, :receiver, user: true, foreign_key: true
+    #add_reference :chats, :sender, user: true, foreign_key: true
+    #add_reference :chats, :receiver, user: true, foreign_key: true
   end
 end
