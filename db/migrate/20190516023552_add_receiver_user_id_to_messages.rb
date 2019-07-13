296100554208a943f5e91e15ff3b8cb0b81d94f6
Nicolas Mateo Casas Ramos <nmcasasr@unal.edu.co>
2019-07-13 14:01:54 -0500

    correcion migraciones chat

--- db/migrate/20190516023552_add_receiver_user_id_to_messages.rb
+++ db/migrate/20190516023552_add_receiver_user_id_to_messages.rb
@@ -1,5 +1,5 @@
 class AddReceiverUserIdToMessages < ActiveRecord::Migration[5.2]
   def change
-    add_column :chats, :receiver_user_id, :integer
+    #add_column :chats, :receiver_user_id, :integer
   end
 end
